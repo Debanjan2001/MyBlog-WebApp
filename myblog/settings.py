ad6107882997ccaee9c5a65650dbdb07c3a36b54
Debanjan Saha <debanjansaha2001@gmail.com>
2021-04-21 15:58:02 +0530

    Added Settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%*q^j1pyksr=6@lb#l@e!t9-_(&gegk48kk1nxx2^=(q-ghxsg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://debanjan2001.pythonanywhere.com/']
 
 
 # Application definition
