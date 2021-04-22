fe27b1d30fb5600963b0457b1dcfb62deeb6b905
Debanjan Saha <debanjansaha2001@gmail.com>
2021-04-23 00:38:21 +0530

    Added LoginRequiredMixins

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,13 @@
-from django.db.models.base import Model
 from django.views.generic.base import TemplateView
 from . forms import CommentForm, PostForm
-from typing import Generic
-from django.http.response import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls.base import reverse_lazy
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from . models import Post,Comment
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
 
@@ -17,11 +16,11 @@ from django.core.paginator import Paginator
 class LoginPage(LoginView):
     template_name = 'admin/login.html'
 
-class LogoutPage(LogoutView):
+class LogoutPage(LoginRequiredMixin,LogoutView):
     template_name = 'admin/logout.html'
 
 
-class CreateNewPostView(CreateView):
+class CreateNewPostView(LoginRequiredMixin,CreateView):
     model = Post
     template_name = 'blog/post_form.html'
     form_class = PostForm
@@ -45,13 +44,14 @@ class PostListView(ListView):
 #     model = Post
 #     context_object_name = 'post'
 
-#     def get_queryset(self): 
+#     def get_queryset(self):
 #         print(self.request)
 #         post = get_object_or_404(Post,pk = self.kwargs['pk'])
 #         # post.comments = post.comments.all().order_by('created_date').reverse()
 #         return post;
 
 # Class based view is not comfortable in this case
+
 def PostDetailView(request,pk):
     post = get_object_or_404(Post,pk= pk)
     context = {}
@@ -61,7 +61,7 @@ def PostDetailView(request,pk):
 
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin,UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_edit.html'
@@ -69,18 +69,19 @@ class PostUpdateView(UpdateView):
     redirect_field_name = 'blog/post_detail.html'
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     template_name = 'blog/post_delete_confirm.html'
     success_url = reverse_lazy('blog:home')
     context_object_name = 'post'
 
+@login_required
 def publish_post_view(request,pk):
     post = get_object_or_404(Post,pk = pk)
     post.publish()
     return redirect('blog:home')
 
-class DraftList(ListView):
+class DraftList(LoginRequiredMixin,ListView):
     paginate_by = 3
     model = Post
     template_name = 'blog/draft.html'
@@ -117,19 +118,20 @@ def CommentListView(request,pk):
     context['page_obj'] = page_obj
     return render(request,'blog/comment_list.html',context=context)
 
-class CommentDeleteView(DeleteView):
+class CommentDeleteView(LoginRequiredMixin,DeleteView):
     model = Comment
     template_name = 'blog/comment_delete_confirm.html'
     context_object_name = 'comment'
-    
+
     def get_success_url(self):
         return reverse_lazy('blog:detail',kwargs = {'pk': self.object.post.pk })
-    
-
-
-    
 
 
 
-    
+
+
+
+
+
+
 
